1f8c76eef8e9e30a9904e4790f6721b84e20adb4
ibrahimnassar24 <ebrahim24111990@gmail.com>
2025-06-25 03:20:50 +0300

    app: initial cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Store } from '@ngrx/store';
-import * as userActions from './state/user/user.actions';
-import SignUserForm from './helpers/signUserForm';
-import { AuthApiService } from './services/auth-api.service';
-import * as statusActions from './state/status/status.actions';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,36 +15,9 @@ export class AppComponent {
   title = 'connect4-frontend';
 
   constructor(
-    private authApiService: AuthApiService,
-    private store: Store
   ) {
 
   }
 
-  register() {
-    const temp: SignUserForm = {
-      email: "user@test.com",
-      password: "123@Bc",
-    };
 
-    this.store.dispatch(userActions.register(temp));
-  }
-
-  login() {
-    const temp: SignUserForm = {
-      email: "user@test.com",
-      password: "123@Bc",
-    };
-
-    this.store.dispatch(userActions.logIn(temp));
-  }
-
-  logout() {
-    this.store.dispatch(userActions.logOut());
-  }
-
-  test() {
-    // this.authApiService.test();
-    this.store.dispatch(statusActions.navigateTo({ path: "profile"}));
-  }
 }
